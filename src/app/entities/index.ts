c2d3ccc59a055276857cb8566921329e440f8d9a
marlon bagans <m.bagans@outlook.de>
2025-07-09 20:05:31 +0200

    double not better

--- src/app/entities/index.ts
+++ src/app/entities/index.ts
@@ -2,4 +2,3 @@ export { User } from './user.entity';
 export { Deck } from './deck.entity';
 export { Game } from './game.entity';
 export { User_deck } from './user_deck.entity';
-export { User } from './user.entity';
