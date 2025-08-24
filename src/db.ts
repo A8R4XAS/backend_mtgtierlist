725cd44205134566d74cf38ca4f683ae834b5097
marlon bagans <m.bagans@outlook.de>
2025-08-24 17:22:05 +0200

    rebuild for env files using

--- src/db.ts
+++ src/db.ts
@@ -1,24 +1,29 @@
 
 import { Config } from '@foal/core';
 import { DataSource } from 'typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
-export function createDataSource(): DataSource {
-  return new DataSource({
-    type: Config.getOrThrow('database.type', 'string') as any,
+const env = Config.get('.env'); // 'development' | 'test' | 'production'
+const isProd = env === 'production';
 
-    url: Config.get('database.url', 'string'),
-    host: Config.get('database.host', 'string'),
-    port: Config.get('database.port', 'number'),
-    username: Config.get('database.username', 'string'),
-    password: Config.get('database.password', 'string'),
-    database: Config.get('database.database', 'string'),
+const dbConfig: Partial<PostgresConnectionOptions> = isProd 
+  ? {
+      type: 'postgres',
+      url: Config.getOrThrow('database.url', 'string'),
+      ssl: { rejectUnauthorized: false }, // Render -> selbstsigniertes SSL-Zertifikat 
+  } 
+: {
+      type: Config.getOrThrow('database.type', 'string') as any,
+      host: Config.getOrThrow('database.host', 'string'),
+      port: Number(Config.getOrThrow('database.port', 'string')), // Zahl konvertieren
+      username: Config.getOrThrow('database.username', 'string'),
+      password: Config.getOrThrow('database.password', 'string'),
+      database: Config.getOrThrow('database.database', 'string'),
+  };
 
-    dropSchema: Config.get('database.dropSchema', 'boolean', false),
-    synchronize: Config.get('database.synchronize', 'boolean', false),
-
-    entities: ['build/app/**/*.entity.js'],
-    migrations: ['build/migrations/*.js'],
-  });
-}
-
-export const dataSource = createDataSource();
+export const dataSource = new DataSource({
+  ...dbConfig,
+  synchronize: Config.get('database.synchronize', 'boolean', true),
+  entities: ['build/app/**/*.entity.js'],
+  migrations: ['build/migrations/*.js'],
+} as PostgresConnectionOptions);
