a038f38587f71655bb9f6b69de4b532fbcdc51ac
marlon bagans <m.bagans@outlook.de>
2025-09-03 19:50:51 +0200

    add corsPreMiddleware so every route is in there

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,34 @@ import { Config, createApp, Logger, ServiceManager } from '@foal/core';
 // App
 import { AppController } from './app/app.controller';
 import { dataSource } from './db';
+import { BaseEntity } from 'typeorm';
 
 console.log('Database host (from Config):', Config.get('database.host'));
 
+const corsPreMiddleware = (req: any, res: any, next: any) => {
+  const origin = req.headers.origin;
+  if (origin === 'http://localhost:5173') {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Vary', 'Origin');
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+  res.header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+};
+
 async function main() {
   await dataSource.initialize();
+  BaseEntity.useDataSource(dataSource);
 
   const serviceManager = new ServiceManager();
   const logger = serviceManager.get(Logger);
 
-  const app = await createApp(AppController, { serviceManager });
+  const app = await createApp( AppController,{ serviceManager, preMiddlewares: [corsPreMiddleware]});
 
   const port = Config.get('port', 'number', 3001);
   app.listen(port, () => logger.info(`Listening on port ${port}...`));
