3fddabe62f65b94d871f3bf4e4f01c93b80e2e1e
marlon bagans <m.bagans@outlook.de>
2025-09-04 15:05:04 +0200

    test again

--- src/db.ts
+++ src/db.ts
@@ -4,25 +4,19 @@ import { Config } from '@foal/core';
 import * as entities from './app/entities';
 import { DatabaseSession } from '@foal/typeorm';
 
-// Prod vs Dev erkennen
-const env = process.env.SETTINGS_ENV?.toLowerCase() === 'production' ? 'production' : 'development';
-
-
+// db.ts jetzt ohne Dev/Prod Unterscheidung, alles kommt aus Config/Env
 const dbConfig: Partial<PostgresConnectionOptions> = {
   type: 'postgres',
-  host: env === 'production' ? process.env.DB_HOST : Config.get('database.host', 'string'),
-  port: env === 'production' ? Number(process.env.DB_PORT) || 5432 : Config.get('database.port', 'number', 5432),
-  username: env === 'production' ? process.env.DB_USERNAME : Config.get('database.username', 'string'),
-  password: env === 'production' ? process.env.DB_PASSWORD : Config.get('database.password', 'string'),
-  database: env === 'production' ? process.env.DB_NAME : Config.get('database.database', 'string'),
-  synchronize:
-    env === 'production'
-      ? process.env.DB_SYNCHRONIZE === 'true'
-      : Config.get('database.synchronize', 'boolean', true),
-  ssl: env === 'production' ? { rejectUnauthorized: false } : undefined, // Render Postgres SSL
+  host: Config.get('database.host', 'string'),
+  port: Config.get('database.port', 'number'),
+  username: Config.get('database.username', 'string'),
+  password: Config.get('database.password', 'string'),
+  database: Config.get('database.database', 'string'),
+  synchronize: Config.get('database.synchronize', 'boolean'),
+  ssl: process.env.SETTINGS_ENV === 'production' ? { rejectUnauthorized: false } : undefined, // SSL nur in Prod
 };
 
-console.log('Database config:', { ...dbConfig});
+console.log('Database config:', { ...dbConfig });
 
 export const dataSource = new DataSource({
   ...dbConfig,
