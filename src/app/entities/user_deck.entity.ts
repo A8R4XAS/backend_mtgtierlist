90bca58707ac84f61ac327edb22be637556e1d51
marlon bagans <m.bagans@outlook.de>
2025-09-21 13:15:49 +0200

    add commentary

--- src/app/entities/user_deck.entity.ts
+++ src/app/entities/user_deck.entity.ts
@@ -17,18 +17,23 @@ export class User_deck extends BaseEntity {
   @JoinColumn()
   deck: Deck;
 
+  //Games where this deck user combination was player 1
   @OneToMany(() => Game, game => game.user_deck1)
   games1: Game[];
 
+  //Games where this deck user combination was player 2
   @OneToMany(() => Game, game => game.user_deck2)
   games2: Game[];
 
+  //Games where this deck user combination was player 3
   @OneToMany(() => Game, game => game.user_deck3)
   games3: Game[];
 
+  //Games where this deck user combination was player 4
   @OneToMany(() => Game, game => game.user_deck4)
   games4: Game[];
 
+  //Games where this deck user combination was the winner
   @OneToMany(() => Game, game => game.winner)
   winner: Game[];
 
